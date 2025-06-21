faf5b3b694e7768aea595a1a46b33d04a0a2afd5
Ricard Dalmau <rdalmau@gmail.com>
2025-06-21 02:06:17 +0200

    changes cnv cnv2

--- js/main.js
+++ js/main.js
@@ -9,34 +9,27 @@ import { initUI } from './ui.js';
 
 
 export let cnv; // p5.js canvas element
-export let cnv2;  // no se quin es el canvas de veritat. 
 export const snapToGrid = true; 
-
-
-
-// ─── Firebase Auth ──────────────────────────────────────────────────────
-
 
 
 // ─── p5.js setup & draw ─────────────────────────────────────────────────
 export function setup() {
-  cnv2 = createCanvas(800, 600);
+  cnv = createCanvas(800, 600);
   noLoop();
 
   initUI();
 
   // Prevent right-click menu
-  cnv = document.querySelector('canvas');   // cnv es un p5.Element
-  cnv.addEventListener('contextmenu', e => e.preventDefault());
+  let cnv2 = document.querySelector('canvas');   // cnv es un p5.Element
+  cnv2.addEventListener('contextmenu', e => e.preventDefault());
 
   // Prevent page scroll on canvas wheel
   //cnv.addEventListener('wheel', e => e.preventDefault(), { passive: false });
-  cnv.addEventListener('wheel', handleZoom, { passive: false });
+  cnv2.addEventListener('wheel', handleZoom, { passive: false });
 
-  // Hidden file input for “Load” (L). 
+    // Hidden file input for “Load” (L). 
   // It is created here because it is a p5.js specific function
   setFileInput(createFileInput(handleFile));
- 
 }
 
 // Bind p5’s globals
